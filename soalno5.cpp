6ac8c6a7abf18e7fdda1a11c10e1975ccb582928
Irsyadiyyah <irsyasitialirsyadiyyah@gmail.com>
2017-11-08 00:23:44 +0700

    membuat percabangan dan proses untuk menentutukan hasil

--- soalno5.cpp
+++ soalno5.cpp
@@ -10,5 +10,11 @@ cout << "Silahkan masukkan tinggi badan(cm) : ";
 cin >>t;
 cout << "Silahkan masukkan berat badan(kg) : " ;
 cin >> b;
+if (b<t/2.5)
+cout << "  Underweight" << endl;
+else if (t/2.3 < b)
+cout << "  Overweight" << endl;
+else if (t/2.5 <= b <= t/2.3)
+cout << "  Normal" << endl;
 
 }
