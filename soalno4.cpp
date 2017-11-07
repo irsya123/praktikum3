c1f5730b12e30556d520fed55f60da288fc20c25
Irsyadiyyah <irsyasitialirsyadiyyah@gmail.com>
2017-11-08 00:19:03 +0700

    membuat percabangan untuk mengetahui angka yg di input positif negaatif atau nol

--- soalno4.cpp
+++ soalno4.cpp
@@ -5,4 +5,16 @@ int main(){
 	int angka;
 	
 	cout<<"Input angka : ";cin>>angka;
+	if (angka < 0)
+	{
+		cout<<"bilangan yang anda input negatif"<<endl;
+	}
+    else if (angka>0)
+	{
+		cout<<"bilangan yang anda input positif"<<endl;
+	}
+    else
+	
+		cout<<"bilangan yang anda input nol "<<endl;
+
 }
