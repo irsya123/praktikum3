db9233226f3fd575a08bb313839cf9c65361ca9a
Irsyadiyyah <irsyasitialirsyadiyyah@gmail.com>
2017-11-08 00:14:44 +0700

    membuat proses perhitungan dan output dari perhitungan

--- soalno3.cpp
+++ soalno3.cpp
@@ -11,4 +11,14 @@ int main(){
 	int gb;
 	cout <<"Masukan jam kerja anda:"; cin >> jk;
 	cout <<"Masukan gaji anda:"; cin >> gpj;
+	l=jk-40;
+	jl=l*1.5;
+	jkt=40+jl;
+	gt=jkt*gpj;
+	gb=gt-(0.15*gt);
+	cout <<"jam lembur anda:"<<l<<endl;
+	cout <<"bonus dari lembur anda:"<<jl<<endl;
+	cout <<"jam kerja total anda:"<<jkt<<endl;
+	cout <<"gaji total anda:"<<gt<<endl;
+	cout <<"gaji bersih anda di potong pajak:"<<gb<<endl;
 }
